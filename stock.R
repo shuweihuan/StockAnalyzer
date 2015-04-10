c61e0590e6a0f0ee3fdf5656ca42905b7d3763c1
shuweihuan <weihuan.shu@gmail.com>
2015-04-10 17:42:07 +0800

    update

--- stock.R
+++ stock.R
@@ -6,5 +6,13 @@ data2xts <- function(data) {
 	data$Adj.High <- round(data$High * data$Adj.Close / data$Close, 2)
 	data$Adj.Low <- round(data$Low * data$Adj.Close / data$Close, 2)
 	columns <- c("Open", "Close", "High", "Low", "Volume", "Adj.Open", "Adj.Close", "Adj.High", "Adj.Low")
-	head(xts(data[columns], order.by=data$Date))
+	x <- xts(data[columns], order.by=data$Date)
+	x$Index <- matrix(1:nrow(x))
+	# x$Adj.Close.Diff.Daily <- x$Adj.Close - lag(x$Adj.Close)
+	x$Adj.Close.Diff.Daily <- diff(x$Adj.Close, lag=1)
+	x[1:20]
 }
+
+# source("stock.R")
+# bidu <- read.csv("bidu.20150410.csv")
+# b <- data2xts(bidu)
